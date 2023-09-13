288bbea6a5a49257da67f027dde2f3a7ff52c0a3
Alumno <Alumno@LAB1504-05>
2023-09-12 22:00:58 -0500

    Agregar archivos de proyecto.

---
+++ Student-Datatable/Program.cs
@@ -0,0 +1,110 @@
+﻿//Librerias del ADO .NET
+using System.Data.SqlClient;
+using System.Data;
+using System.Reflection.PortableExecutable;
+using WpfApp1;
+
+class Program
+{
+    // Cadena de conexión a la base de datos
+    public static string connectionString = "Data Source=LAB1504-05\\SQLEXPRESS;Initial Catalog=TecsupDB;User ID=alumnoTecsup;Password=123456";
+
+
+    static void Main()
+    {
+
+        #region FormaDesconectada
+        //Datatable
+        DataTable dataTable = ListarStudentsDataTable();
+       
+       
+       Console.WriteLine("Lista de Estudiantes:");
+       foreach (DataRow row in dataTable.Rows)
+       {
+           Console.WriteLine($"ID: {row["StudentId"]}, Nombre: {row["FirstName"]}, Apellido: {row["LastName"]}");
+       }
+        #endregion
+
+
+        #region FormaConectada
+        //Datareader
+        List<Student> students = ListarStudents();
+        foreach (var item in students)
+        {
+            Console.WriteLine($"ID: {item.StudentId}, Nombre: {item.FirstName}, Apellido: {item.LastName}");
+        }
+        #endregion
+    }
+
+    //De forma desconectada
+    private static DataTable ListarStudentsDataTable()
+    {
+        // Crear un DataTable para almacenar los resultados
+        DataTable dataTable = new DataTable();
+        // Crear una conexión a la base de datos
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            // Abrir la conexión
+            connection.Open();
+
+            // Consulta SQL para seleccionar datos
+            string query = "SELECT * FROM Students";
+
+            // Crear un adaptador de datos
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+
+
+
+            // Llenar el DataTable con los datos de la consulta
+            adapter.Fill(dataTable);
+
+            // Cerrar la conexión
+            connection.Close();
+
+        }
+        return dataTable;
+    }
+    //De forma conectada
+    private static List<Student> ListarStudents()
+    {
+        List<Student> students = new List<Student>();
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            // Abrir la conexión
+            connection.Open();
+
+            // Consulta SQL para seleccionar datos
+            string query = "SELECT StudentId,FirstName,LastName FROM Students";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    // Verificar si hay filas
+                    if (reader.HasRows)
+                    {
+                        Console.WriteLine("Lista de Estudiantes:");
+                        while (reader.Read())
+                        {
+                            // Leer los datos de cada fila
+
+                            students.Add(new Student
+                            {
+                                StudentId = (int)reader["StudentId"],
+                                FirstName = reader["FirstName"].ToString(),
+                                LastName = reader["LastName"].ToString()
+                            });
+
+                        }
+                    }
+                }
+            }
+            // Cerrar la conexión
+            connection.Close();
+        }
+        return students;
+    }
+
+
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        private string connectionString = "Data Source=LAB1504-05\\SQLEXPRESS;Initial Catalog=TecsupDB;User ID=alumnoTecsup;Password=123456";
+        private List<Student> studentsList;
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeDatabaseConnection();
+        }
+
+        private void InitializeDatabaseConnection()
+        {
+            studentsList = new List<Student>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("SELECT * FROM Students", connection))
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            studentsList.Add(new Student
+                            {
+                                StudentId = reader.GetInt32(0),
+                                FirstName = reader.GetString(1),
+                                LastName = reader.GetString(2)
+                            });
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar a la base de datos: " + ex.Message);
+            }
+
+            studentsDataGrid.ItemsSource = studentsList;
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string searchTerm = searchTextBox.Text.Trim();
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                List<Student> filteredList = studentsList.FindAll(student =>
+                    student.FirstName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                    student.LastName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase)
+                );
+                studentsDataGrid.ItemsSource = filteredList;
+            }
+            else
+            {
+                studentsDataGrid.ItemsSource = studentsList;
+            }
+        }
+    }
+
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+}
